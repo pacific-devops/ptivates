aaee07fecf720835d8d89ddd7f26f1f5ef1e5c04
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-21 23:14:00 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,6 @@
 module.exports = {
   "branches": ["main"],
+  "tagFormat": "${packageName}-v${version}",,
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
