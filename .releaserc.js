00a198e09ed76b6180dadbab095a585161ca9f16
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-24 10:43:01 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -4,14 +4,8 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    [
-      "@semantic-release/github",
-      {
-        "assets": [
-          {"path": "CHANGELOG.md", "label": "Changelog"}
-        ]
-      }
-    ]
+    "@semantic-release/github",
+    "@semantic-release/git"
   ],
   "extends": "semantic-release-monorepo",  // Use semantic-release-monorepo for monorepo handling
   tagFormat: "v${version}",  // Set format for version tags
