0780ca421f3c80c3dcf22e7577744d26cc5d8de3
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-25 09:58:28 +0530

    Update .multi-releaserc.js

--- .multi-releaserc.js
+++ .multi-releaserc.js
@@ -1,15 +1,16 @@
 const path = require("path");
 const packageJson = require(path.resolve(process.cwd(), "package.json"));
+
 module.exports = {
-  "branches": [
-    "main"
-  ],
-  "plugins": [
+  branches: ["main"], // Replace "main" with your branch name
+  tagFormat: `${packageJson.name}-v${"${version}"}`, // Proper semantic-release version placeholder
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/git"
+    "@semantic-release/git",
   ],
-  tagFormat: `${packageJson.name}-v${version}` // Correctly interpolates version
 };
+
