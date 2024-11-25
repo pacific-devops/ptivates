3222e1bfb43ea622258f41a22cacb24fba56c711
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-25 21:30:05 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,13 +1,42 @@
 module.exports = {
-  "branches": ["main"],
-  "tagFormat": "${packageName}-v${version}",
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/github",
-    "@semantic-release/git"
+  branches: [
+    { name: "main" },
+    { name: "feature/*", channel: "dev-feature", prerelease: '${name.replace("feature/", "dev-")}' }
   ],
-  "extends": "semantic-release-monorepo"
+  tagFormat: "v${version}",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer", 
+      { "preset": "conventionalcommits" }
+    ],
+    [
+      "@semantic-release/release-notes-generator", 
+      { "preset": "conventionalcommits" }
+    ],
+    [
+      "@semantic-release/changelog", 
+      { "changelogFile": "CHANGELOG.md" }
+    ],
+    [
+      "@semantic-release/exec", 
+      {
+        generateNotesCmd: `
+          echo "PREV_TAG=v${lastRelease.version}" >> $GITHUB_OUTPUT;
+          echo "NEXT_TAG=v${nextRelease.version}" >> $GITHUB_OUTPUT;
+          if [ "${process.argv.slice(2)[0]}" != "" ]; then 
+            echo "### Artifact Reference"; 
+            echo "* JFrog Artifact link ([${process.argv.slice(2)[0]}](${process.argv.slice(2)[1]}))"; 
+          fi
+        `
+      }
+    ],
+    "@semantic-release/git",
+    [
+      "@semantic-release/github", 
+      {
+        "successComment": false, 
+        "failTitle": false 
+      }
+    ]
+  ]
 };
-
