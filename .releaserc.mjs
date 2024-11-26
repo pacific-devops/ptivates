23f081d52fa0b113b3a53c43a965af9c178a1eb5
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-26 10:45:21 +0530

    Update .releaserc.mjs

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -21,6 +21,7 @@ const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
       plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
+        "@semantic-release/monorepo",
         [
           "@semantic-release/changelog",
           {
