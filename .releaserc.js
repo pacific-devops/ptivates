bcd97ae2f3a778ed0c67f6115d56f22e5bc75809
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-26 08:40:15 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,21 +1,24 @@
 const path = require("path");
 const packageJson = require(path.resolve(process.cwd(), "package.json"));
+
+// Capture custom input from command line
+const customInput = process.argv[2] || "default-value";
+
 module.exports = {
-  "branches": ["main"],
-  "plugins": [
+  branches: ["main"],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: `chore(release): ${packageJson.name} v\${nextRelease.version} (${customInput}) [skip ci]`,
+      },
+    ],
     "@semantic-release/github",
-    "@semantic-release/git"
   ],
-  "extends": "semantic-release-monorepo",  // Use semantic-release-monorepo for monorepo handling
-  //tagFormat: "v${version}",  // Set format for version tags
-  tagFormat: `${packageJson.name}-v${"${version}"}`
-}
-
-
-
-
-
-
+  extends: "semantic-release-monorepo",
+  tagFormat: `${packageJson.name}-v${"${version}"}`,
+};
