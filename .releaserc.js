7289658d7aa46ee82bdb9c1286b772ae194bdde2
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-29 22:08:12 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -2,25 +2,45 @@ const path = require("path");
 const packageJson = require(path.resolve(process.cwd(), "package.json"));
 
 module.exports = {
-  branches: ["main"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/github",
-    "@semantic-release/git",
-     [
-      "@semantic-release/exec",
-        {
-          generateNotesCmd: `
-               echo "### Artifact Reference" >> release-notes.md;
-              echo "* JFrog Artifact link ([hardcoded-artifact.zip](https://jfrog.com/hardcoded-artifact.zip))" >> release-notes.md;
-        `,
-      },
-    ]
-
-  ],
+branches: ["main"],
+plugins: [
+          [
+            "@semantic-release/commit-analyzer",
+            { preset: "conventionalcommits" },
+          ],
+          [
+            "@semantic-release/release-notes-generator",
+            { preset: "conventionalcommits" },
+          ],
+          [
+            "@semantic-release/changelog",
+            { changelogFile: "CHANGELOG.md" },
+          ],
+          [
+            "@semantic-release/exec",
+            {
+              generateNotesCmd: `
+                echo "PREV_TAG=v${lastRelease.version}" >> $GITHUB_OUTPUT;
+                echo "NEXT_TAG=v${nextRelease.version}" >> $GITHUB_OUTPUT;
+                echo "RELEASE_TYPE=${nextRelease.type}" >> $GITHUB_OUTPUT;
+                if [ "${jFrogFileName}" != "" ]; then
+                  echo "### Artifact Reference" >> release-notes.md;
+                  echo "* JFrog Artifact link ([${jFrogFileName}](${jFrogFileUrl}))" >> release-notes.md;
+                fi
+              `,
+            },
+          ],
+          "@semantic-release/git",
+          [
+            "@semantic-release/github",
+            {
+              successComment: false,
+              failTitle: false,
+            },
+          ],
+        ],
   extends: "semantic-release-monorepo",  // Use semantic-release-monorepo for monorepo handling
   //tagFormat: "v${version}",  // Set format for version tags
   tagFormat: `${packageJson.name}-v${"${version}"}`
 }
+
