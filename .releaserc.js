d72c582b66cbaf48b88a30ea86677768331fb42c
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-11-29 19:45:53 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,15 +1,27 @@
 const path = require("path");
 const packageJson = require(path.resolve(process.cwd(), "package.json"));
+
 module.exports = {
-  "branches": ["main"],
-  "plugins": [
+  branches: ["main"],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/github",
-    "@semantic-release/git"
+    "@semantic-release/git",
+    [
+      "@semantic-release/exec",
+      {
+        generateNotesCmd: `
+          if (process.env.JFROG_FILE_NAME && process.env.JFROG_FILE_URL) {
+            echo "### Artifact Reference" >> release-notes.md;
+            echo "* JFrog Artifact link ([${process.env.JFROG_FILE_NAME}](${process.env.JFROG_FILE_URL}))" >> release-notes.md;
+          }
+        `
+      }
+    ]
   ],
-  "extends": "semantic-release-monorepo",  // Use semantic-release-monorepo for monorepo handling
+  extends: "semantic-release-monorepo",  // Use semantic-release-monorepo for monorepo handling
   //tagFormat: "v${version}",  // Set format for version tags
   tagFormat: `${packageJson.name}-v${"${version}"}`
 }
